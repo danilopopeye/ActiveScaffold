2a9a6ab8fa0e116555803aff7f101e64b247a2e3
Danilo Sousa <danilopopeye@gmail.com>
2009-12-31 21:32:59 -0200

    Added the templates and some actions;

--- activeScaffold.php
+++ activeScaffold.php
@@ -1,41 +1,144 @@
-<?php
+m<?php
 define('BASEPATH','../');
+define('DS',DIRECTORY_SEPARATOR);
+
+// TODO: find a better place to put the include :(
+include( BASEPATH . '..'. DS .'helpers'. DS .'inflector_helper.php');
 
 class ActiveScaffold {
-    private $actions = array('M','C','V');
-    private $conn;
-    private $args;
+    var $actions = array('M','MODEL','C','CONTROLLER');
+    var $conn;
+    var $args;
+    var $reserved = array(
+        'Controller', 'CI_Base', '_ci_initialize', '_ci_scaffolding', 'index'
+    );
 
-    public function run( $args ){
+    function run( $args ){
+        array_shift( $args );
+
         $this->args = $args;
 
-        $this->getAction();
-    }
-
-    private function getAction(){
-        switch ( count($this->args) ){
+        switch( count( $this->args ) ){
             case 2:
-                
-            break;
+                $this->getAction(
+                    $this->parseAction( $this->args[0] ), $this->args[1]
+                ); break;
 
             case 1:
-            default: 
-                $this->menu();
-            break;
+                
+                break;
+
+            default:
+                echo 'menu';
+                break;
         }
     }
 
-    private function menu(){
-        echo " ActiveScaffold Console\n";
+    function getAction( $type, $name = FALSE ){
+        if( ! $type ){
+            // TODO: Redirect to menu
+            echo "Invalid action!";
+            return FALSE;
+        }
+
+        $name = $this->parseName( $name );
+
+        if( $this->getInput( 'Build a '. $type .' called "'. $name .'"', array('y','n') ) == 'n' ){
+            // TODO: Redirect to menu
+            return FALSE;
+        }
+
+        // TODO: validar se a resposta e boleana
+        if( $this->save( $name, $type ) === TRUE ){
+            echo ucwords( $type ) .' salvo!';
+        }
+        
+        
+    }
+    
+    function save( $name, $type ){
+        $dir = BASEPATH . $type . 's';
+
+        if( ! is_dir( $dir ) ){
+            return FALSE;
+        }
+
+        // TODO: Colocar a validacao antes
+        if( is_file( $dir . DS . $name . '.php' ) ){
+            echo 'Ja existe um ' . $type . ' chamado ' . $name;
+            return FALSE;
+        }
+
+        $template = $this->parseTemplate( $name, $type );
+
+        if( $template === FALSE ){
+            echo 'Erro ao parsear o template do ' . $type . ' ' . $name;
+            return FALSE;
+        }
+
+        $isOk = @file_put_contents( $dir . DS . strtolower( $name ) . '.php', $template );
+
+        if( $isOk === FALSE ){
+            echo 'Erro ao salvar o arquivo';
+            return FALSE;
+        }
+
+        return TRUE;
+    }
+
+    function getInput( $message, $options = "" ){
+        if( is_array( $options ) ){
+            $options = '[ ' . implode( ' | ', $options ) .' ]';
+        }
+
+        echo ' '. $message .'? '. $options ."\n:";
+
+        return strtolower( trim( fgets( STDIN ) ) );
+    }
+
+    function parseAction( $a ){
+        $a = strtoupper( $a );
+
+        if( ! in_array( $a, $this->actions ) ){
+            return FALSE;
+        }
+
+        if( $a == 'M' OR $a == 'MODEL' ){
+            return 'model';
+        } else if ( $a == 'C' OR $a == 'CONTROLLER' ){
+            return 'controller';
+        }
+    }
+
+    // TODO: mudar o retorno para array com nome do arquivo e conteudo
+    function parseTemplate($name, $template){
+        $buff = @file_get_contents( $template );
+
+        if( $buff === FALSE ){
+            return FALSE;
+        }
+
+        $buff = str_replace( '{name}', ucwords( $name ), $buff );
+
+        $buff = str_replace( '{fileName}', strtolower( $name ), $buff );
+
+        return $buff;
+    }
+
+    function parseName( $name ){
+        return strtolower( $name );
+    }
+
+    function help(){
+        echo " ActiveScaffold Help\n";
         echo " -------------------------\n";
         echo " [M]odel\n";
         echo " [C]controller\n";
-        echo " -------------------------\n";
-        echo " What do you wanna do ?\n";
+        echo " -------------------------\n\n";
     }
 
-    private function connect(){
-        include( BASEPATH . 'config/database.php' );
+    function connect(){
+        include( BASEPATH . 'config'. DS .'database.php' );
 
         if( ! isset($active_group) OR ! isset($db) OR count($db) == 0 ){
             return FALSE;
